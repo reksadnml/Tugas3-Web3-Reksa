27c1e2a78b605edbc7c8c06a0bb9280630bdd846
Reksa Milenia <reksa.milenia@gmail.com>
2020-03-24 00:04:25 +0700

    Updates

---
+++ precache-manifest.9d23ee5d24b5408343d855e5d770d1c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3778407220737268d196cf4e2a91e6d0",
+    "url": "/WEB3B-425975-REKSA-REACT/index.html"
+  },
+  {
+    "revision": "b62a2462e81d60197d0a",
+    "url": "/WEB3B-425975-REKSA-REACT/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "a5d725282717ff5f20af",
+    "url": "/WEB3B-425975-REKSA-REACT/static/css/main.b04a3c3d.chunk.css"
+  },
+  {
+    "revision": "b62a2462e81d60197d0a",
+    "url": "/WEB3B-425975-REKSA-REACT/static/js/2.6fc11baa.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/WEB3B-425975-REKSA-REACT/static/js/2.6fc11baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d725282717ff5f20af",
+    "url": "/WEB3B-425975-REKSA-REACT/static/js/main.9cf8c57b.chunk.js"
+  },
+  {
+    "revision": "590376d495d53b986076",
+    "url": "/WEB3B-425975-REKSA-REACT/static/js/runtime-main.d7f742b2.js"
+  },
+  {
+    "revision": "b69b5e71a4a4d2c8af54fda64bb16f96",
+    "url": "/WEB3B-425975-REKSA-REACT/static/media/picture.b69b5e71.svg"
+  }
+]);
